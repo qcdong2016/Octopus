c5214682442632e0a29d10d19eba5a6e2e8c5120
qcdong <qcdong@yeah.net>
2022-05-12 10:08:10 +0800

    system_tray to local

---
+++ Octopus/system_tray-0.1.0/linux/tray.h
@@ -0,0 +1,48 @@
+#ifndef __Tray_H__
+#define __Tray_H__
+
+#include <gtk/gtk.h>
+#include <libappindicator/app-indicator.h>
+
+typedef AppIndicator* (*app_indicator_new_fun)(const gchar*,
+                                               const gchar*,
+                                               AppIndicatorCategory);
+typedef void (*app_indicator_set_status_fun)(AppIndicator*, AppIndicatorStatus);
+typedef void (*app_indicator_set_icon_full_func)(AppIndicator* self,
+                                                 const gchar* icon_name,
+                                                 const gchar* icon_desc);
+typedef void (*app_indicator_set_attention_icon_full_fun)(AppIndicator*,
+                                                          const gchar*,
+                                                          const gchar*);
+typedef void (*app_indicator_set_menu_fun)(AppIndicator*, GtkMenu*);
+
+class SystemTray {
+ public:
+  bool init_system_tray(const char* title,
+                        const char* iconPath,
+                        const char* toolTip);
+
+  bool set_system_tray_info(const char* title,
+                            const char* iconPath,
+                            const char* toolTip);
+
+  bool set_context_menu(GtkWidget* system_menu);
+
+ protected:
+  bool init_indicator_api();
+  bool create_indicator(const char* title,
+                        const char* iconPath,
+                        const char* toolTip);
+
+ protected:
+  app_indicator_new_fun _app_indicator_new = nullptr;
+  app_indicator_set_status_fun _app_indicator_set_status = nullptr;
+  app_indicator_set_icon_full_func _app_indicator_set_icon_full = nullptr;
+  app_indicator_set_attention_icon_full_fun
+      _app_indicator_set_attention_icon_full = nullptr;
+  app_indicator_set_menu_fun _app_indicator_set_menu = nullptr;
+
+  AppIndicator* _app_indicator = nullptr;
+};
+
+#endif  // __Tray_H__
---
+++ Octopus/system_tray-0.1.0/macos/Classes/Tray.swift
@@ -0,0 +1,134 @@
+import FlutterMacOS
+
+let kDefaultSizeWidth = 18
+let kDefaultSizeHeight = 18
+
+let kSystemTrayEventCallbackMethod = "SystemTrayEventCallback"
+
+let kSystemTrayEventLButtnUp = "leftMouseUp"
+let kSystemTrayEventLButtnDown = "leftMouseDown"
+let kSystemTrayEventRButtnUp = "rightMouseUp"
+let kSystemTrayEventRButtnDown = "rightMouseDown"
+
+class SystemTray: NSObject, NSMenuDelegate {
+  var statusItem: NSStatusItem?
+  var statusItemMenu: NSMenu?
+  var channel: FlutterMethodChannel
+
+  init(_ channel: FlutterMethodChannel) {
+    self.channel = channel
+  }
+
+  func menuDidClose(_ menu: NSMenu) {
+    statusItem?.menu = nil
+  }
+
+  @objc func onSystemTrayEventCallback(sender: NSStatusBarButton) {
+    if let event = NSApp.currentEvent {
+      switch event.type {
+      case .leftMouseUp:
+        channel.invokeMethod(kSystemTrayEventCallbackMethod, arguments: kSystemTrayEventLButtnUp)
+      case .leftMouseDown:
+        channel.invokeMethod(kSystemTrayEventCallbackMethod, arguments: kSystemTrayEventLButtnDown)
+      case .rightMouseUp:
+        channel.invokeMethod(kSystemTrayEventCallbackMethod, arguments: kSystemTrayEventRButtnUp)
+      case .rightMouseDown:
+        channel.invokeMethod(kSystemTrayEventCallbackMethod, arguments: kSystemTrayEventRButtnDown)
+      default:
+        break
+      }
+    }
+  }
+
+  func initSystemTray(title: String?, base64Icon: String?, toolTip: String?) -> Bool {
+    statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+
+    statusItem?.button?.action = #selector(onSystemTrayEventCallback(sender:))
+    statusItem?.button?.target = self
+    statusItem?.button?.sendAction(on: [
+      .leftMouseUp, .leftMouseDown, .rightMouseUp, .rightMouseDown,
+    ])
+
+    if let toolTip = toolTip {
+      statusItem?.button?.toolTip = toolTip
+    }
+
+    if let title = title {
+      statusItem?.button?.title = title
+    }
+
+    if let base64Icon = base64Icon {
+      if let imageData = Data(base64Encoded: base64Icon, options: .ignoreUnknownCharacters),
+        let itemImage = NSImage(data: imageData)
+      {
+        let destSize = NSSize(width: kDefaultSizeWidth, height: kDefaultSizeHeight)
+        itemImage.size = destSize
+        statusItem?.button?.image = itemImage
+        statusItem?.button?.imagePosition = NSControl.ImagePosition.imageLeft
+      }
+    }
+
+    return true
+  }
+
+  func setSystemTrayInfo(title: String?, base64Icon: String?, toolTip: String?) -> Bool {
+    if let toolTip = toolTip {
+      statusItem?.button?.toolTip = toolTip
+    }
+
+    if let title = title {
+      statusItem?.button?.title = title
+    }
+
+    if let base64Icon = base64Icon {
+      if let imageData = Data(base64Encoded: base64Icon, options: .ignoreUnknownCharacters),
+        let itemImage = NSImage(data: imageData)
+      {
+        let destSize = NSSize(width: kDefaultSizeWidth, height: kDefaultSizeHeight)
+        itemImage.size = destSize
+        statusItem?.button?.image = itemImage
+        statusItem?.button?.imagePosition = NSControl.ImagePosition.imageLeft
+      } else {
+        statusItem?.button?.image = nil
+      }
+    }
+
+    return true
+  }
+
+  func setContextMenu(menu: NSMenu) -> Bool {
+    statusItemMenu = menu
+    statusItemMenu?.delegate = self
+    return true
+  }
+
+  func popUpContextMenu() -> Bool {
+    if let statusItemMenu = statusItemMenu {
+      statusItem?.menu = statusItemMenu
+      statusItem?.button?.performClick(nil)
+      return true
+    }
+    return false
+  }
+
+  func setPressedImage(base64Icon: String?) {
+    if let base64Icon = base64Icon {
+      if let imageData = Data(base64Encoded: base64Icon, options: .ignoreUnknownCharacters),
+        let itemImage = NSImage(data: imageData)
+      {
+        let destSize = NSSize(width: kDefaultSizeWidth, height: kDefaultSizeHeight)
+        itemImage.size = destSize
+        statusItem?.button?.alternateImage = itemImage
+        statusItem?.button?.setButtonType(.toggle)
+      } else {
+        statusItem?.button?.alternateImage = nil
+      }
+    } else {
+      statusItem?.button?.alternateImage = nil
+    }
+  }
+
+  func getTitle() -> String {
+    return statusItem?.button?.title ?? ""
+  }
+}
